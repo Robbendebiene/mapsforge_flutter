9b81d8fca449c6e795047236e9e83852788fa891
Robbendebiene <robbendebiene@mailbox.org>
2021-03-04 17:58:08 +0100

    updated platform specific files

---
+++ indoor_example/android/app/src/main/kotlin/com/indoor/indoor_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.indoor.indoor_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ indoor_example/flutter console.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export CHROME_EXECUTABLE=/usr/lib/chromium/chromium
+
+export PATH=$PATH:/home/robin/Dokumente/Studium/Master/Masterarbeit/Tools/flutter/bin
+
+# Flutter Doctor
+flutter doctor
+
+$SHELL
